a7e818b84f1946a212001469a4e5bbf8fe3a7245
otsutsukirajo-dev <otsutsukirajo@gmail.com>
2026-07-21 23:05:19 +0300

    Renouvellement

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\Security\Security;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
